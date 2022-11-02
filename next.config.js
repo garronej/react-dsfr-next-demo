a2f35f6df2f55e4107aa7938ed028bd8f497ca20
garronej <joseph.garrone@data.gouv.fr>
2022-11-02 17:11:58 +0100

    Update to Next 13

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withTM = require('next-transpile-modules')(['@codegouvfr/react-dsfr']);
 module.exports = withTM({
   reactStrictMode: true,
   webpack: config => {
-
     config.module.rules.push({
       test: /\.(woff2|webmanifest)$/,
       type: "asset/resource"
