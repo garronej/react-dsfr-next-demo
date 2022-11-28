21c1ceff19c462b9d2a82e14fc5d6bdf9dc6783e
garronej <joseph.garrone@data.gouv.fr>
2022-11-29 00:00:18 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import DefaultApp from "next/app";
+import type { AppProps } from "next/app";
 import { createNextDsfrIntegrationApi } from "@codegouvfr/react-dsfr/next";
+import { createEmotionSsrAdvancedApproach } from "tss-react/next";
+import { Header } from "@codegouvfr/react-dsfr/Header";
+import { fr } from "@codegouvfr/react-dsfr";
+import { DsfrLangProvider } from "@codegouvfr/react-dsfr";
+import { createMuiDsfrThemeProvider, noAugmentation } from "@codegouvfr/react-dsfr/mui";
 
 const { withDsfr, dsfrDocumentApi } = createNextDsfrIntegrationApi({
-	"defaultColorScheme": "system",
+	"defaultColorScheme": "light",
 	"preloadFonts": [
 		//"Marianne-Light",
 		//"Marianne-Light_Italic",
@@ -18,6 +23,57 @@ const { withDsfr, dsfrDocumentApi } = createNextDsfrIntegrationApi({
 	"doPersistDarkModePreferenceWithCookie": true
 });
 
+const { withAppEmotionCache, augmentDocumentWithEmotionCache } = createEmotionSsrAdvancedApproach({
+	"key": "css"
+});
+
+export { augmentDocumentWithEmotionCache };
+
 export { dsfrDocumentApi };
 
-export default withDsfr(DefaultApp);
+const { MuiDsfrThemeProvider } = createMuiDsfrThemeProvider({
+	"augmentMuiTheme": noAugmentation
+});
+
+function App({ Component, pageProps }: AppProps) {
+
+	return (
+		<DsfrLangProvider lang="fr" >
+			<MuiDsfrThemeProvider>
+				<Header
+					intituléOfficiel="Intitulé officiel"
+					baselinePrécisionsSurLorganisation="baseline - Précision sur l'organisation"
+					nomDuSiteSlashService="Nom du site / service"
+					links={[
+						{
+							"text": "Créer un espace",
+							"iconId": "fr-icon-add-circle-line",
+							"href": "#"
+						},
+						{
+							"text": "Se connecter",
+							"iconId": "fr-icon-lock-line",
+							"href": "#"
+						},
+						{
+							"text": "S'enregistrer",
+							"iconId": "fr-icon-account-line",
+							"href": "#"
+						}
+					]}
+				/>
+				<div style={{
+					"margin": "auto",
+					"maxWidth": 1000,
+					...fr.spacing("padding", { "topBottom": "10v" })
+				}}>
+					<Component {...pageProps} />
+				</div>
+			</MuiDsfrThemeProvider>
+		</DsfrLangProvider>
+	);
+
+
+}
+
+export default withDsfr(withAppEmotionCache(App));
