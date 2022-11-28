305b830e6cb42a6e71f0ddb67fe3cc68921b93fc
garronej <joseph.garrone@data.gouv.fr>
2022-11-29 00:30:37 +0100

    clean setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
 import { dsfrDocumentApi } from "./_app";
-import { augmentDocumentWithEmotionCache } from "./_app";
 
 const {
 	augmentDocumentByReadingColorSchemeFromCookie,
@@ -20,5 +19,3 @@ export default function Document(props: DocumentProps) {
 }
 
 augmentDocumentByReadingColorSchemeFromCookie(Document);
-
-augmentDocumentWithEmotionCache(Document);
