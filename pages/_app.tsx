9d50a44bca2707df09768a1fd427a01791dcb95e
garronej <joseph.garrone@data.gouv.fr>
2022-11-28 23:10:08 +0100

    Clean

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,23 @@
 import DefaultApp from "next/app";
 import { createNextDsfrIntegrationApi } from "@codegouvfr/react-dsfr/next";
-import { createEmotionSsrAdvancedApproach } from "tss-react/next";
-import "@codegouvfr/react-dsfr/dsfr/dsfr.css";
-import "@codegouvfr/react-dsfr/dsfr/utility/icons/icons.css";
 
 const { withDsfr, dsfrDocumentApi } = createNextDsfrIntegrationApi({
-		"defaultColorScheme": "system",
-		"preloadFonts": [
-			//"Marianne-Light",
-			//"Marianne-Light_Italic",
-			"Marianne-Regular",
-			//"Marianne-Regular_Italic",
-			"Marianne-Medium",
-			//"Marianne-Medium_Italic",
-			"Marianne-Bold",
-			//"Marianne-Bold_Italic",
-			//"Spectral-Regular",
-			//"Spectral-ExtraBold"
-		],
-		"doPersistDarkModePreferenceWithCookie": true
+	"defaultColorScheme": "system",
+	"preloadFonts": [
+		//"Marianne-Light",
+		//"Marianne-Light_Italic",
+		"Marianne-Regular",
+		//"Marianne-Regular_Italic",
+		"Marianne-Medium",
+		//"Marianne-Medium_Italic",
+		"Marianne-Bold",
+		//"Marianne-Bold_Italic",
+		//"Spectral-Regular",
+		//"Spectral-ExtraBold"
+	],
+	"doPersistDarkModePreferenceWithCookie": true
 });
 
 export { dsfrDocumentApi };
 
-const { 
-	augmentDocumentWithEmotionCache, 
-	withAppEmotionCache 
-} = createEmotionSsrAdvancedApproach({ "key": "css" });
-
-export { augmentDocumentWithEmotionCache };
-
-export default withDsfr(withAppEmotionCache(DefaultApp));
+export default withDsfr(DefaultApp);
