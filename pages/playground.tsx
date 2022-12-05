5adef6840538773fc4b44af28aaf6a2c9714545f
garronej <joseph.garrone@data.gouv.fr>
2022-12-05 05:44:26 +0100

    update

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -1,10 +1,12 @@
 
 import { Alert } from "@codegouvfr/react-dsfr/Alert";
+import { fr } from "@codegouvfr/react-dsfr";
 
 export default function Playground() {
 
 	return (
 		<Alert
+			className={fr.cx("fr-m-10v")}
 			severity="success"
 			isClosable
 			description="Everything went well"
